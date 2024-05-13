3c00d85bde87c6bda39330df5b04cfc1d75edd83
‘HarshitChejara’ <harshitchejara29@gmail.com>
2024-05-13 16:22:53 +0530

    changes

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -3,9 +3,47 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
+    // <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    //   <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
+    //     <span>
+    //       <Heading
+    //         level="h1"
+    //         className="text-3xl leading-10 text-ui-fg-base font-normal"
+    //       >
+    //         Mactriq Ecommerce Starter
+    //       </Heading>
+    //       <Heading
+    //         level="h2"
+    //         className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+    //       >
+    //         Powered by Medusa and Next.js
+    //       </Heading>
+    //     </span>
+    //     <a
+    //       href="https://github.com/medusajs/nextjs-starter-medusa"
+    //       target="_blank"
+    //     >
+    //       <Button variant="secondary">
+    //         View on GitHub
+    //         <Github />
+    //       </Button>
+    //     </a>
+    //   </div>
+    // </div>
+
+
+
+
+
+
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
+        <img
+          src="/hero1.jpg"
+          alt="Hero Image"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
           <Heading
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
